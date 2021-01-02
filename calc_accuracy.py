30ce46681b13c4f42e5fbe485a655ae9d57afd95
durbek kosimov <26488170+durbek-kosimov@users.noreply.github.com>
2021-01-02 18:31:09 +0100

    Create calc_accuracy.py
    Calculating the Accuracy

---
+++ calc_accuracy.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+music_data = pd.read_csv('music.csv')
+
+# input data frame
+X = music_data.drop(columns=['genre']) 
+
+# output data frame
+y = music_data['genre'] 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+predictions = model.predict(X_test)
+
+score = accuracy_score(y_test, predictions)
+score
