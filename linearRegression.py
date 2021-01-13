33f666e307255689c1880ed4b7d519ed969291b6
durbek kosimov <26488170+durbek-kosimov@users.noreply.github.com>
2021-01-13 15:12:01 +0100

    Linear Regression

--- linearRegression.py
+++ linearRegression.py
@@ -16,6 +16,27 @@ data = pd.read_csv('headbrain.csv')
 print(data.shape)
 data.head()
 
+# Collecting X and Y
+x = data['Head Size(sm3)'].values
+y = data['Brain Weight(gramms)'].values
+
+# mean X and Y
+mean_x = np.mean(x)
+mean_y = np.mean(y)
+
+# Total number of values
+m = len(x)
+
+# Using the formula to calculate b1 and b2
+numer = 0
+denom = 0
+for i in range(m):
+    numer += (x[i] - mean_x) * (y[i] - mean_y)
+    denom += (x[i] - mean_x) ** 2
+b1 = numer / denom # slope m
+b0 = mean_y - (b1 * mean_x)
+print(b1, b0)
+
 
 # In[ ]:
 
